5cb77cc908973c4e47fd0853e588c9b77410d2ce
Sjuniperus <sapindacea@gmail.com>
2024-10-07 15:45:40 +0200

    database.php & config.php

--- index.php
+++ index.php
@@ -6,13 +6,10 @@ require "functions.php";
 
 require "Database.php";
 
+$config = require "config.php";
+$db = new Database($config['database']);
 
-
-$db = new Database();
-
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
-
-
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
