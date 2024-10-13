a4ded962b30d6775045174e0f2e309de41ce581f
Sjuniperus <sapindacea@gmail.com>
2024-10-13 20:07:04 +0200

    refactoring Router & routes, routes delete&store

--- Core/functions.php
+++ Core/functions.php
@@ -10,6 +10,13 @@ function dd($value) {
     die();
 }
 
+function abort($status = 404){
+    http_response_code($code);
+        require base_path("views/{$code}.php");
+        die();
+
+}
+
 
 function urlIs($value){
     return $_SERVER['REQUEST_URI'] === $value;
