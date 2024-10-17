9696b926e847a5bf3abefa100b9583b21f3b672a
Sjuniperus <sapindacea@gmail.com>
2024-10-17 18:47:05 +0200

    hash password

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -43,7 +43,7 @@ if ($user) {
     //If not, save one to the db, and then log the user in, and redirect.
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)',[
         'email' =>$email,
-        'password'=> $password
+        'password'=> password_hash($password, PASSWORD_BCRYPT)
     ]);
 
     //mark that the user has logged in.
